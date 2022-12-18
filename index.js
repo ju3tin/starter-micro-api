8c95d8ce81529c88a1d9891471422fee37adea35
Justin <ju3tin@hotmail.co.uk>
2022-12-18 03:11:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo! hi there');
-    res.end();
-}).listen(process.env.PORT || 3000);
+ var express = require('express');
+    var https = require('https');
+    var fs = require('fs');
+
+    var options = {
+      ca: [fs.readFileSync(PATH_TO_BUNDLE_CERT_1), fs.readFileSync(PATH_TO_BUNDLE_CERT_2)],
+      cert: fs.readFileSync(PATH_TO_CERT),
+      key: fs.readFileSync(PATH_TO_KEY)
+    };
+
+    app = express()
+
+    app.get('/', function(req,res) {
+        res.send('hello');
+    });
+
+    var server = https.createServer(options, app);
+
+    server.listen(8001, function(){
+        console.log("server running at https://IP_ADDRESS:8001/")
+    });
